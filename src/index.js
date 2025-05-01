09ddcf0d9f1bfcade072cdc53ce6a9da665a6b48
Sumit Singh <sumitsingh3076@gmail.com>
2025-05-01 16:22:30 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const fastify = require('fastify')({
+    logger: true
+})
+
+
+// db:
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost//fastify')
+    .then(() => console.log("MONGO is ready!!"))
+    .catch(error => console.log(error))
+
+
+
+
+
+// routes:
+fastify.get('/', async (request, reply) => {
+    return { visitor: "Hello devs!! try fastify , this is faster than express" }
+})
+
+
+
+const address = 8000;
+// starting server
+const start = async () => {
+    try {
+        fastify.listen({ port: 8000 })
+        fastify.log.info(`Server is running at http://localhost:${address}`)
+    } catch (error) {
+        fastify.log.error("Error occured", error)
+    }
+}
+
+
+start();
